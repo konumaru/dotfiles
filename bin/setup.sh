a4b6b90969669f7f66ef1cc3dfe933051292dc25
konumaru <konumaru1022@gmail.com>
2021-08-11 11:50:13 +0900

    [change] no output brew lock file

--- bin/setup.sh
+++ bin/setup.sh
@@ -21,7 +21,7 @@ if type brew >/dev/null 2>&1; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 ## Install packages from Brewfile
-brew bundle --file=Brewfile
+brew bundle --file=Brewfile --no-lock
 
 
 # Creat symbolic links
