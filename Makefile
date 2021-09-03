f814e847e87e5cd15358e7f25b4a09cc39df69cc
konumaru <konumaru1022@gmail.com>
2021-09-03 15:49:13 +0900

    [backup] 2021_09_03

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
+today = ${shell date '+%Y_%m_%d'}
+
 setup:
 	bash -c "$$(curl -L https://raw.githubusercontent.com/konumaru/dotfiles/main/bin/setup.sh)"
 
 backup:
 	bash -c bin/backup.sh
+
+	git add -A
+	git commit -m "[backup] ${today}"
+	git push origin main
--- bin/setup.sh
+++ bin/setup.sh
@@ -19,7 +19,7 @@ fi
 # Install Homebrew
 ## Install homebrew when it was not installed
 if type brew >/dev/null 2>&1; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 ## Install packages from Brewfile
 brew bundle --file=Brewfile --no-lock
