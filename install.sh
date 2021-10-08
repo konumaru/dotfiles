ffe2fcfbbde45999ed1933290b9a2f9c0c7e7056
konumaru <konumaru1022@gmail.com>
2021-10-08 09:49:39 +0900

    mv file

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!bin/bash
+
+DOTPATH=~/.dotfiles
+GITHUB_URL=https://github.com/konumaru/dotfiles
+
+# Clone this repository
+if type "git" > /dev/null 2>&1; then
+  if [ -d $DOTPATH ]; then
+    cd $DOTPATH && git pull
+  else
+    git clone ${GITHUB_URL}.git $DOTPATH
+  fi
+else
+  echo "Install git!"
+  exit 1
+fi
+
+case ${OSTYPE} in 
+    darwin*)
+        echo "Running on MacOS"
+        #TODO: run install script of macos.
+        ;;
+    linux*)
+        if   [ -e /etc/debian_version ] || [ -e /etc/debian_release ]; then
+            # Check Ubuntu or Debian
+            if [ -e /etc/lsb-release ]; then
+            # Ubuntu
+            echo "Running on Ubuntu"
+            distri_name="ubuntu"
+        else
+            # Other
+            echo "unkown distribution"
+            distri_name="unkown"
+        fi
+esac
