fd2026ce78280076463e5f2392df03697114639a
konumaru <konumaru1022@gmail.com>
2021-10-09 10:51:22 +0900

    [update] setup some apps

--- etc/ubuntu/install_apps.sh
+++ etc/ubuntu/install_apps.sh
@@ -7,3 +7,27 @@ sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /et
 sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt update
 sudo apt install google-chrome-stable
+
+
+# ==================================================
+# Spotify
+# ==================================================
+snap install spotify
+
+
+# ==================================================
+# 1password
+# ==================================================
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/amd64 stable main' | sudo tee /etc/apt/sources.list.d/1password.list
+sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+sudo apt -y update && sudo apt install -y 1password
+
+
+# ==================================================
+# VSCode
+# ==================================================
+sudo snap install --classic code
