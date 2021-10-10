cb1e12eb6842e6a8b795ebc8cb461044acfea7ec
konumaru <konumaru1022@gmail.com>
2021-10-10 10:03:56 +0900

    [backup] 2021_10_10

--- etc/ubuntu/setup.sh
+++ etc/ubuntu/setup.sh
@@ -17,16 +17,29 @@ sudo apt install -y \
     libopencv-dev \
     tk-dev \
     curl \
+    ibus-mozc \
     python3-pip \
     python3-dev \
     neovim \
     tmux \
     tree \
+    gnome-tweak-tool \
+    powertop tlp tlp-rdw
     fonts-noto-cjk \
-    fonts-noto-cjk-extra
-
+    fonts-noto-cjk-extra \
+    ttf-mscorefonts-installer \
+    fonts-roboto \
+    fonts-noto \
+    fonts-ricty-diminished
 
 apt autoremove -y
+
+ibus restart
+gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'jp'), ('ibus', 'mozc-jp')]"
+
+# Install font, jetbrains mono.
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+
 
 # Change default shell to zsh.
 chsh -s $(which zsh)
