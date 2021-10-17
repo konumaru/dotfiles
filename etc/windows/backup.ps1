b576af3b3a6535f45e06bcc1db24440e1d939e60
konumaru <konumaru1022@gmail.com>
2021-10-17 16:11:11 +0900

    [backup] winget apps

--- etc/windows/backup.ps1
+++ etc/windows/backup.ps1
@@ -1,7 +1,7 @@
-winget export --output \etc\windows\winget_packages.json
+winget export --output .\backups\windows\winget_packages.json
 
 # NOTE: タスクバーのレイアウトを保存
 # 新規ユーザーの場合のみ適用可能なのでバックアップのみ
-Export-StartLayout -Path '.\etc\windows\start_layout.xml'
+# Export-StartLayout -Path '.\etc\windows\start_layout.xml'
 
 
