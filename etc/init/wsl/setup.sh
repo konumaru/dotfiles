04643841acc17714e30e0d242bda7a6ee7c623bc
konumaru <konumaru1022@gmail.com>
2024-10-10 09:46:37 +0900

    Refactor WSL setup script: Remove unnecessary code and directories

--- etc/init/wsl/setup.sh
+++ etc/init/wsl/setup.sh
@@ -91,10 +91,6 @@ nvm install --lts
 sudo mkdir ${HOME}/.config
 sudo mkdir ${HOME}/.config/nvim
 
-# Set wsl config
-windowsUserProfile=/mnt/c/Users/$(cmd.exe /c "echo %USERNAME%" 2>/dev/null | tr -d '\r')
-cp ${HOME}/dotfiles/.wslconfig ${windowsUserProfile}
-
 # Make dirs
 sudo mkdir ${HOME}/Documents
 sudo mkdir ${HOME}/Documents/repositories
