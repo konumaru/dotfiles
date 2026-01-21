f631c308b4ff3a2f2f7af2e59845d22f1b7c6fba
konumaru <konumaru1022@gmail.com>
2026-01-21 12:19:20 +0900

    feat: 設定ファイルのデプロイ処理を改善

---
+++ .config/nvim/lua/plugins/csvview.lua
@@ -0,0 +1,11 @@
+return {
+  {
+      'hat0uma/csvview.nvim',
+      config  = function()
+          require('csvview').setup{
+              -- Your configuration comes here
+              -- or leave it empty to use the default settings
+          }
+      end
+  }
+}
